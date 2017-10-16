fede9456b75d7ac9a00e26da40ac3c428eb17b91
fov42550564 <42550564@qq.com>
2017-10-16 11:43:02 +0800

    update to 1.0.8

--- android/src/main/java/com/remobile/splashscreen/RCTSplashScreenPackage.java
+++ android/src/main/java/com/remobile/splashscreen/RCTSplashScreenPackage.java
@@ -30,7 +30,6 @@ public class RCTSplashScreenPackage implements ReactPackage {
         );
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
